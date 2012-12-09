517648b64230cda50f5eab04da0a14ef8f14b5be
Marcelo Oliveira <askoth@gmail.com>
2012-12-09 01:31:23 -0200

    using json file again

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,17 +2,17 @@
 
 
 $.when(
-	$.ajax('javascripts/templates/rawdata.html'),
-	// $.ajax('javascripts/data.json'),
+	// $.ajax('javascripts/templates/rawdata.html'),
+	$.ajax('javascripts/data.json'),
 	$.ajax('javascripts/templates/list.html')
 ).then(function(rawData, listTemplate){ 
-	var dataList = $(rawData[0]).find('tbody tr'),
-	// var dataList = rawData[0],
+	// var dataList = $(rawData[0]).find('tbody tr'),
+	var dataList = rawData[0],
 		output = rawData,
 		dataArray = [];
 
-	dataArray = parseDataList(dataList);
-	// dataArray = rawData[0];
+	// dataArray = parseDataList(dataList);
+	dataArray = rawData[0];
 
 	output = template(listTemplate[0], {listItems: dataArray});
 
